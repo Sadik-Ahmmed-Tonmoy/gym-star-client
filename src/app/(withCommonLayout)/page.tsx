5f9101e7ca6a0c3a414fdc4f31c7a9c90170cf24
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2024-10-18 16:30:55 +0600

    home update

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -15,8 +15,8 @@ const HomePage = () => {
 
   return (
     <div>
-      <Card2/>
-      <Card2/>
+      {/* <Card2/>
+      <Card2/> */}
       <HorizontalScroll />
       <div className="bg-primary-foreground py-10 rounded-md">
         <h1 className="font-departure text-4xl relative z-10 text-center h-[120px] md:h-auto leading-tight">
